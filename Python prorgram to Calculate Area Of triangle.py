150b5e1fec89a306931e42ce389a7599c051a7ab
Abhishek Kumar <103246309+Abhikhush28@users.noreply.github.com>
2023-07-21 07:10:07 +0530

    Create Python prorgram to Calculate Area Of triangle.py

---
+++ Python prorgram to Calculate Area Of triangle.py
@@ -0,0 +1,7 @@
+# forluma of Area Of Triangle is (1/2)*base*height
+height = float(input("Enter the hight of the Triangle"))
+base = float(input("Enter the base of the Triangle"))
+
+
+Area = (1/2)*base*height
+print("The Area of the Triangle is", Area)
