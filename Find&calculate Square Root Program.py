da8a7800f8d2640c2178b7f2ac77e029eb7ed23a
Abhishek Kumar <103246309+Abhikhush28@users.noreply.github.com>
2023-07-21 07:05:55 +0530

    Update Find&calculate Square Root Program.py

--- Find&calculate Square Root Program.py
+++ Find&calculate Square Root Program.py
@@ -1,1 +1,15 @@
+# Solution using Exponential 
+# Solution Using Exponenetial 
+# num = 64
+# num1 = int(input("Enter the number: "))
 
+# sr = num1**(1/2)
+
+# print("The square root of the number is", sr)
+
+#Solution Using Math module
+
+import math
+num = int(input("Enter the number: "))
+sr = math.sqrt(num)
+print("the Square Root of the number ", sr)
