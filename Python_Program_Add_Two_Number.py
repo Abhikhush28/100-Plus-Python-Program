91d0e57caa255440fb8d8de19f53e5c5c679788d
Abhishek Kumar <103246309+Abhikhush28@users.noreply.github.com>
2023-07-21 06:46:23 +0530

    Update Python_Program_Add_Two_Number.py

--- Python_Program_Add_Two_Number.py
+++ Python_Program_Add_Two_Number.py
@@ -1,5 +1,12 @@
-# solution
-num1 = 12
-num2 = 34
+# solution with predefine variable
+# num1 = 12
+# num2 = 34
+# sum = num1 + num2
+# print("Sum of the Two Number" , sum)
+
+#solution with the user input
+num1 = float(input("Enter the First Number"))
+num2 = float(input("Enter the Second Number"))
 sum = num1 + num2
-print("Sum of the Two Number" , sum)
+
+print("The total sum of the these two number are", sum)
