b0f7b50e19a26702fa72506e2d63f3d7afe5333f
Abhishek Kumar <103246309+Abhikhush28@users.noreply.github.com>
2023-07-21 07:40:50 +0530

    Update # Python program to Convert kilometer to miles.py

--- # Python program to Convert kilometer to miles.py
+++ # Python program to Convert kilometer to miles.py
@@ -9,3 +9,19 @@ conversation_unit = 0.62137
 Miles = kilometer_1 * conversation_unit
  
 print("The number of miles is ", Miles)
+
+
+# Python program to Convert kilometer to miles 
+# 1 kilometer equals 0.62137
+# so, Miles = kilometer * 0.62137
+# and , kilometer = miles/0.62137
+# using Function
+
+def kilometer_2(km):
+    conversation_ratio = 0.62137
+    miles = km * conversation_ratio
+    
+    print("The speed of the car in the value of miles", miles)
+
+kilometer3 = int(input("Enter the speed of the car as kilometer"))
+kilometer_2(kilometer3)
